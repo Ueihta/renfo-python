d62ba9401f42cdf8e362b8ba39c19f7b3d9de65a
Ueihta <mathieugonzalez62@gmail.com>
2025-09-24 15:55:13 +0200

    idk
    je n'arrive pas à savoir ce qu'il faut mettre

--- ecole/usecase/ajouter_cours_a_un_eleve/ajouter_cours_a_un_eleve.py
+++ ecole/usecase/ajouter_cours_a_un_eleve/ajouter_cours_a_un_eleve.py
@@ -10,7 +10,3 @@ class AjouterCoursAUnEleve:
     def executer(self, eleve_id: int, cours_id: int) -> list[CoursEntite]:
         eleve = self.eleve_repository.recuperer_eleve_par_id(eleve_id)
         cours = self.cours_repository.recuperer_cours_par_id(cours_id)
-        if eleve and cours:
-            eleve_cours = eleve.cours
-            eleve_cours.append(cours)   
-            return eleve_cours
