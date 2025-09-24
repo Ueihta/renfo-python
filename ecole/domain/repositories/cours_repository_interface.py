827182d29ae1ec8ceca4c0e1ecdea789a85df413
Ueihta <mathieugonzalez62@gmail.com>
2025-09-24 13:48:12 +0200

    binary file changed

--- ecole/domain/repositories/cours_repository_interface.py
+++ ecole/domain/repositories/cours_repository_interface.py
@@ -9,3 +9,7 @@ class CoursRepositoryInterface(ABC):
     @abstractmethod
     def recupere_cours_par_professeur_id(self, professeur_id: int) -> list[CoursEntite]:
         pass
+
+    @abstractmethod
+    def recuperer_cours_par_id(self, cours_id: int) -> list[CoursEntite] :
+        pass
--- ecole/domain/repositories/eleve_repository_interface.py
+++ ecole/domain/repositories/eleve_repository_interface.py
@@ -5,3 +5,7 @@ class EleveRepositoryInterface(ABC):
     @abstractmethod
     def recuperer_tous_les_eleves(self) -> list[EleveEntite]:
         pass
+
+    @abstractmethod
+    def recuperer_eleve_par_id(self, eleve_id: int) -> EleveEntite:
+        pass
